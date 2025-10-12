ad6a1a399ea9c304eaadb25c7ebeef413a9e621d
pushpendra-407 <pushpendramangroliya05@gmail.com>
2025-10-12 19:24:31 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header, { Profile } from "./Header"
+
+function App(){
+  return <div>
+    <Header/>
+    <Profile />
+  </div>
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+function Header(){
+  return <h3>Header file</h3>
+}
+
+export function Profile(){
+  return <div>
+    <h2>
+      Profile
+    </h2>
+  </div>
+}
+
+export default Header
