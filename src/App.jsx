0a219e3dd6ed3929cd4aff791a4c3cfe12d7d497
pushpendra-407 <pushpendramangroliya05@gmail.com>
2025-10-12 19:32:53 -0400

    react component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Header, { Profile } from "./Header"
 
 function App(){
+  const userName = "Pushpendra"
+  let x = 10
+  let y = 20
   return <div>
-    <Header/>
-    <Profile />
+    {/* <Header/>
+    <Profile /> */}
+    <h1>{userName}</h1>
+    <h4>Sum of 10 + 20 is {x+y}</h4>
+    <button onClick={()=> alert("Hello")}>Click Me</button>
+
   </div>
 }
 
