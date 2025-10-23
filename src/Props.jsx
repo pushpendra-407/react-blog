58f02ca314c8f8e668de160c2fdc4f966bec6ce7
pushpendra-407 <pushpendramangroliya05@gmail.com>
2025-10-23 18:34:05 -0400

    Radio Button and Dropdown Examples

--- src/Props.jsx
+++ src/Props.jsx
@@ -1,73 +1,87 @@
-import { useState } from "react"
-import Wrapper from "./Wrapper"
-
+import { useState } from "react";
+import Wrapper from "./Wrapper";
 
 const Props = ({ user }) => {
-
-  const [userName, setuserName] = useState("")
-  const [age, setAge] = useState()
-  const [university, setUniversity] = useState("")
+  const [userName, setuserName] = useState("");
+  const [age, setAge] = useState();
+  const [university, setUniversity] = useState("");
 
   const userNameValue = () => {
+    return (
+      <div>
+        <label htmlFor="userName">Username: </label>
+        <input
+          type="text"
+          onChange={(event) => setuserName(event.target.value)}
+          placeholder="Enter Username"
+          value={userName}
+        />
+      </div>
+    );
+  };
+
+  const ageValue = () => {
+    return (
+      <div>
+        <label htmlFor="age">age: </label>
+        <input
+          type="number"
+          onChange={(event) => setAge(event.target.value)}
+          placeholder="Enter Age"
+          value={age}
+        />
+      </div>
+    );
+  };
+
+  const universityValue = () => {
+    return (
+      <div>
+        <label htmlFor="university">University Name: </label>
+        <input
+          type="text"
+          onChange={(event) => setUniversity(event.target.value)}
+          placeholder="Enter University Name"
+          value={university}
+        />
+      </div>
+    );
+  };
+
   return (
     <div>
-      <label htmlFor="userName">Username: </label>
-      <input type="text" onChange={(event) => setuserName(event.target.value)} placeholder="Enter Username" value={userName}/>
-      <h1>{userName}</h1>
-    </div>
-  )
-}
+      <div>
+        {userNameValue()}
+        {ageValue()}
+        {universityValue()}
+        <button
+          onClick={() => setuserName("") & setUniversity("") & setAge("")}
+        >
+          Clear the text
+        </button>
 
-const ageValue = () => {
-  return (
-    <div>
-      <label htmlFor="age">age: </label>
-      <input type="number" onChange={(event) => setAge(event.target.value)} placeholder="Enter Age" value={age}/>
-      <h1>{age}</h1>
-    </div>
-  )
-}
+        <h1>{userName}</h1>
+        <h1>{age}</h1>
+        <h1>{university}</h1>
+      </div>
 
-const universityValue = () => {
-  return (
-    <div>
-      <label htmlFor="university">University Name: </label>
-      <input type="text" onChange={(event) => setUniversity(event.target.value)} placeholder="Enter University Name" value={university}/>
-      <h1>{university}</h1>
-    </div>
-  )
-}
+      <div style={{ visibility: "hidden" }}>
+        <Wrapper>
+          <h1>Hello Everyone</h1>
+        </Wrapper>
 
+        <Wrapper>
+          <h1>Hello Guys! Good afternoon</h1>
+        </Wrapper>
 
-  return (
-    <div>
-    <div>
-      {userNameValue()}
-      {ageValue()}
-      {universityValue()}
-    <button onClick={()=>setuserName("") & setUniversity("") & setAge("")}>Clear the text</button>
-
-    </div>
-
-
-
-      <div style={{visibility: "hidden"}}>
-      <Wrapper>
-        <h1>Hello Everyone</h1>
-      </Wrapper>
-
-      <Wrapper>
-        <h1>Hello Guys! Good afternoon</h1>
-      </Wrapper>
-
-      <Wrapper color= "orange">
-        <h1 >Hello Guys! Good afternoon</h1>
-        <h1 >Hello Guys! Good afternoon</h1>
-      </Wrapper>
+        <Wrapper color="orange">
+          <h1>Hello Guys! Good afternoon</h1>
+          <h1>Hello Guys! Good afternoon</h1>
+        </Wrapper>
       </div>
 
       {user.map((person, index) => (
-        <div key={index} style={{visibility:"hidden"}}>
+        <div key={index} style={{ visibility: "hidden" }}>
           <h2>Username: {person.name}</h2>
           <h2>Age: {person.age}</h2>
           <h2>Email: {person.email}</h2>
@@ -76,8 +90,7 @@ const universityValue = () => {
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-
-export default Props
+export default Props;
